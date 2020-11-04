35371e4ebb28b45580b30fbe39723373e0c65ab1
JUDAIhpyer <735845576@qq.com>
2020-11-04 17:03:29 +0800

    update file

--- PySelenium/TestSuite/test_suite_demo.py
+++ PySelenium/TestSuite/test_suite_demo.py
@@ -1,3 +1,15 @@
+"""
+需求：
+    1.利用unittest执行流程测试而非单元测试
+    2.控制unittest的执行顺序
+
+1.unittest.TestSuite() 类来表示一个测试用例集
+    1）用来确定测试用例的顺序，哪个先执行哪个后执行
+    2）如果一个class中有四个test开头的方法，则加载到suite中时则有四个测试用例
+    3）由TestLoader加载TestCase到TestSuite
+    4）verbosity参数可以控制执行结果的输出，0 是简单报告，1 是一般报告， 2 是详细报告
+        默认1 会在每个成功的用例前面有个"." 每个失败的用例前面有个 "F"
+"""
 import unittest
 class MyTestCase(unittest.TestCase):
     def setUp(self):
@@ -23,3 +35,11 @@ class MyTestCase(unittest.TestCase):
     def test_three(self):
         print(" test_three 第三个测试用例")
         self.assertEqual(self.age,32)
+
+if __name__=="__main__":
+    suite=unittest.TestSuite()
+    suite.addTest(MyTestCase("test_one"))
+    suite.addTest(MyTestCase("test_two"))
+    suite.addTest(MyTestCase("test_three"))
+    runner=unittest.TextTestRunner(verbosity=1)
+    runner.run(suite)
